e7281260b515f7b66e2dc5acbe2f9b0068041c8c
ViniKarnopp <karnopp@gmail.com>
2025-02-03 10:23:30 -0300

    Criação da Backend e Criação do Banco e Tabela de Produtos

---
+++ DatabaseScripts/create_table.sql
@@ -0,0 +1,8 @@
+create table products (
+   productid int,
+   descricao varchar(500),
+   preco decimal(10,2),
+   categoria varchar(255),
+   imageurl varchar(255),
+   primary key (productid)
+);
