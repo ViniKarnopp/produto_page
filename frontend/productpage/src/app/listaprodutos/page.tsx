a4d3d1b3f85563d02262ed46b2248d80a016feb7
ViniKarnopp <karnopp@gmail.com>
2025-02-04 08:25:18 -0300

    Frontend Configuração Inicial da Página Home, Página Lista de Produtos, Configuração do Layout e do Header

---
+++ frontend/productpage/src/app/listaprodutos/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ListaProdutos() {
+    return (
+        <div>
+            <h1>Página que Lista os Produtos</h1>
+        </div>
+    );
+}
---
+++ frontend/productpage/src/components/header/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export function Header() {
+    return (
+        <header className="flex px-2 py-4 bg-zinc-900 text-white">
+            <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+                <div>
+                    Produtos
+                </div>
+                <ul className="flex items-center justify-center gap-2">
+                    <li>
+                        <Link href='/'>
+                           Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/listaprodutos'>
+                            Lista de Produtos
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/posts'>
+                             Posts
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
+}
