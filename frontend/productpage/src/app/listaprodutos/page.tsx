777849f4b6369a507dee2da97829b90f3aacd4c4
ViniKarnopp <karnopp@gmail.com>
2025-02-14 11:12:48 -0300

    Frontend Inclusão de campos de filtro na página de ListaProdutos

--- frontend/productpage/src/app/listaprodutos/page.tsx
+++ frontend/productpage/src/app/listaprodutos/page.tsx
@@ -15,8 +15,6 @@ import { ListProducts } from "@/EndPoints/ListProducts";
 
 export default async function ListaProdutos() {
 
-  //const response = await axios.get("https://localhost:7048/api/product");
-  //const response = await handler;
   const response = await ListProducts();
   const data: ProductProps[] = response.data;
 
@@ -26,6 +24,41 @@ export default async function ListaProdutos() {
         <h1 className="text-center mt-5 mb-2 font-bold text-3xl">
             Produtos
         </h1>
+        <h3 className="mx-2">Buscar Produtos</h3>
+        <form className="flex gap-2 my-4 mx-2">
+          <span>Preço Mínimo: </span>
+          <input
+          className="border border-gray-200"
+          type="number"
+          name="PrecoMinimo"
+          min="0"
+          step="0.01"
+          />
+          <span> Preço Máximo: </span>
+          <input
+          className="border border-gray-200"
+          type="number"
+          name="PrecoMaximo"
+          min="0"
+          step="0.01"
+          />
+          <span className="align-center"> Categoria: </span>
+          <select
+          className="border border-gray-200"
+          name="Categoria"
+          >
+            <option value="">Todas</option>
+            <option value="Eletrônico">Eletrônico</option>
+            <option value="Roupas">Roupas</option>
+            <option value="Alimentos">Alimentos</option>
+            <option value="Livros">Livros</option>
+            <option value="Outros">Outros</option>
+          </select>
+          <button
+          className="bg-zinc-900 text-white p-1 rounded-md"
+          type="submit"
+          >Buscar</button>
+        </form>
         <div className="flex flex-col gap-4 mx-2">
             {data.map(p => (
                 <div key={p.ProductId} className="bg-gray-200 p-4 rounded-md">
